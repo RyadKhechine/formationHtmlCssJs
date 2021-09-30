0229107d6720ab3449616633d2658ee63f62f3d4
RyadKhechine <khechineryad@gmail.com>
2021-09-30 16:49:43 +0200

    add fichier: Javascript Tp1

---
+++ javascriptTp1/app.js
@@ -0,0 +1,6 @@
+let nbr1 = parseInt(prompt("entre ton nombre"));
+let nbr2 = parseInt(prompt("entre ton nombre"));
+
+const somme = nbr1 + nbr2;
+alert("Bienvenue sur la page de formulaire");
+alert(`${nbr1} + ${nbr2} = ${somme}`);
